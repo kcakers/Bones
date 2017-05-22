6c7313bb4f56b6e66f74d1e42857fd15dd3f3832
Guzmop <gruntworkgames@gmail.com>
2017-05-22 14:33:19 -0500

    added a hitbox on Left Elbow

---
+++ Assets/Scripts/ClickToZoom.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToZoom : MonoBehaviour {
+
+	private bool touching = false;
+	private float originalFOV;
+
+	public float zoomFOV = 15;
+	public float zoomSpeed = 3.5f;
+
+	void Start () {
+		originalFOV = Camera.main.fieldOfView;
+	}
+
+	void Update () {
+		TouchControl ();
+	}
+
+	private void TouchControl() {
+		if (IsTouching()) { // pressing
+			// if the were not pressing before...
+			if (!touching) {
+				ButtonClicked();
+			} else { // already touching
+				ZoomIn();
+			}
+		} else {
+			if (touching) { // if they were touching...
+				ButtonReleased();
+			}
+			ZoomOut();
+		}
+	}
+
+	private bool IsTouching() {
+		if(Input.GetMouseButtonDown(0) || Input.touchCount > 0 || (Input.GetAxis("Fire1") != 0))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	private void ButtonClicked() {
+		touching = true;
+	}
+
+	private void ButtonReleased() {
+		touching = false;
+	}
+
+	private void ZoomIn() {
+		Camera.main.fieldOfView = Mathf.Lerp (Camera.main.fieldOfView, zoomFOV, zoomSpeed * Time.deltaTime);
+	}
+
+	private void ZoomOut() {
+		Camera.main.fieldOfView = Mathf.Lerp (Camera.main.fieldOfView, originalFOV, zoomSpeed * Time.deltaTime);
+	}
+}
