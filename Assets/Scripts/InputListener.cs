809add37812ec2edaa017021a5a93dfae44465bd
Guzmop <gruntworkgames@gmail.com>
2017-05-30 19:19:56 -0500

    added files

---
+++ Assets/Scripts/InputListener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InputListener : MonoBehaviour {
+
+	private bool touching = false;
+
+	virtual protected void Update () {
+		TouchControl ();
+	}
+
+	private void TouchControl() {
+		if (IsTouching()) { // pressing
+			// if the were not pressing before...
+			if (!touching) {
+				touching = true;
+				ButtonPressed();
+			} else { // already touching
+				ButtonDown();
+			}
+		} else {
+			if (touching) { // if they were touching...
+				touching = false;
+				ButtonReleased();
+			}
+		}
+	}
+
+	protected abstract bool IsTouching ();
+	protected abstract void ButtonPressed ();
+	protected abstract void ButtonReleased ();
+	protected abstract void ButtonDown ();
+}
---
+++ Assets/Scripts/InputToScale.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputToScale : InputListener {
+
+	public Transform target;
+	public float speed;
+	public Vector3 endScale;
+
+	private bool scaling = false;
+	private Vector3 originalScale;
+
+	void Start() {
+		originalScale = target.localScale;
+	}
+
+	protected override void Update () {
+		base.Update ();
+		if (scaling) {
+			ZoomIn ();
+		} else {
+			ZoomOut ();
+		}
+	}
+
+	protected override bool IsTouching () {
+		return Input.GetAxis ("Fire1") != 0;
+	}
+
+	protected override void ButtonPressed () {
+//		print ("ButtonClicked");
+		scaling = true;
+	}
+
+	protected override void ButtonReleased () {
+//		print ("ButtonReleased");
+		scaling = false;
+	}
+
+	protected override void ButtonDown () {
+//		print ("ButtonDown");
+	}
+
+	private void ZoomIn() {
+		target.localScale = Vector3.Lerp (target.localScale, endScale, speed * Time.deltaTime);
+	}
+
+	private void ZoomOut() {
+		target.localScale = Vector3.Lerp (target.localScale, originalScale, speed * Time.deltaTime);
+	}
+}
